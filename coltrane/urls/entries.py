37a4ae5a718289fff20e6557257beaeb36309e9d
Dave Vogler <dave@sparkhouse.com>
2012-11-02 20:20:03 -0400

    Migrated function-based generic views to class-based

--- coltrane/urls/entries.py
+++ coltrane/urls/entries.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic.dates import YearArchiveView, MonthArchiveView, DayArchiveView, DateDetailView
 
 from coltrane.models import Entry
 
@@ -8,11 +9,12 @@ urlpatterns = patterns('',
 # Pagination for the equivalent of archive_index generic view.
 # The url is of the form http://host/page/4/
 # In urls.py for example, ('^blog/page/(?P<page>\d)/$', get_archive_index),
-	(r'^$', 'coltrane.views.get_archive_index_first', ),
-	(r'^page/(?P<page>\d)/$', 'coltrane.views.get_archive_index', ),
+	url(r'^$', 'coltrane.views.get_archive_index_first', ),
+	url(r'^page/(?P<page>\d)/$', 'coltrane.views.get_archive_index', ),
 	#(r'^$', 'django.views.generic.date_based.archive_index', entry_info_dict,  'coltrane_entry_archive_index'),
-	(r'^(?P<year>\d{4})/$', 'django.views.generic.date_based.archive_year', entry_info_dict, 'coltrane_entry_archive_year'),
-	(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', 'django.views.generic.date_based.archive_month', entry_info_dict, 'coltrane_entry_archive_month'),
-	(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', 'django.views.generic.date_based.archive_day', entry_info_dict, 'coltrane_entry_archive_day'),
-	(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', 'django.views.generic.date_based.object_detail', entry_info_dict, 'coltrane_entry_detail'),
+	#(r'^(?P<year>\d{4})/$', YearArchiveView.as_view(), entry_info_dict, 'coltrane_entry_archive_year'),
+	url(r'^(?P<year>\d{4})/$', YearArchiveView.as_view(**entry_info_dict), name= 'coltrane_entry_archive_year'),
+	url(r'^(?P<year>\d{4})/(?P<month>\w{3})/$', MonthArchiveView.as_view(**entry_info_dict), name= 'coltrane_entry_archive_month'),
+	url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$', DayArchiveView.as_view(**entry_info_dict), name= 'coltrane_entry_archive_day'),
+	url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', DateDetailView.as_view(**entry_info_dict), name= 'coltrane_entry_detail'),
 )
