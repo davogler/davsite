874f00fac569102a44a6f80adf6f633d6f63803b
Dave Vogler <dave@sparkhouse.com>
2012-11-04 21:33:14 -0500

    made gunicorn.sh for production, added gunicorn to requirements.

--- conf/gunicorn.sh
+++ conf/gunicorn.sh
@@ -3,13 +3,13 @@ set -e
 
 LOGFILE=/srv/www/davidalanvogler.com/logs/gunicorn_dav.log
 LOGDIR=$(dirname $LOGFILE)
-NUM_WORKERS=1
+NUM_WORKERS=3
 # user/group to run as
-USER=radmin
-GROUP=radmin
+USER=admin
+GROUP=admin
 cd /srv/www/davidalanvogler.com/cms
-source /home/radmin/envs/dav/bin/activate
+source /home/admin/envs/davsite/bin/activate
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /home/radmin/envs/dav/bin/gunicorn_django -w $NUM_WORKERS -b 0.0.0.0:8000\
+exec /home/admin/envs/davsite/bin/gunicorn_django -w $NUM_WORKERS -b 0.0.0.0:8000\
   --user=$USER --group=$GROUP --log-level=debug \
   --log-file=$LOGFILE 2>>$LOGFILE
