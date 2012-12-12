37f810f1fca2aa690c0aa3d8def905c50811325b
Dave Vogler <dave@sparkhouse.com>
2012-12-12 17:16:41 -0500

    added support for responsive video

--- static/js/script.js
+++ static/js/script.js
@@ -70,6 +70,44 @@ David Alan Vogler, 2011
  
  });
  
+ // http://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+ 
+ // Find all YouTube and Vimeo videos
+ var $allVideos = $("iframe[src^='http://player.vimeo.com'], iframe[src^='http://www.youtube.com']"),
+ 
+     // The element that is fluid width
+     $fluidEl = $("figure");
+ 
+ // Figure out and save aspect ratio for each video
+ $allVideos.each(function() {
+ 
+   $(this)
+     .data('aspectRatio', this.height / this.width)
+ 
+     // and remove the hard coded width/height
+     .removeAttr('height')
+     .removeAttr('width');
+ 
+ });
+ 
+ // When the window is resized
+ $(window).resize(function() {
+ 
+   var newWidth = $fluidEl.width();
+ 
+   // Resize all videos according to their own aspect ratio
+   $allVideos.each(function() {
+ 
+     var $el = $(this);
+     $el
+       .width(newWidth)
+       .height(newWidth * $el.data('aspectRatio'));
+ 
+   });
+ 
+ // Kick off one resize to fix all videos on page load
+ }).resize();
+ 
 
 
 });
